fac912297cf278b2bf8227e827ab4be90a4f1160
Judah <87204720+judahtegart@users.noreply.github.com>
2026-01-16 16:51:48 -0800

    Added server info and general updates

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('passwordForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var pw = document.getElementById('password').value;
-    if (pw === 'password123') {
-        window.location.href = '/home';
-    } else {
-        document.getElementById('errorMsg').style.display = 'block';
-    }
-});
